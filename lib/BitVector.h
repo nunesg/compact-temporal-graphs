f75acd3a333bfe9e9d6cecf39fb04ead5f8949d3
Gustavo <gustavonunes14.2011@gmail.com>
2021-05-22 14:14:27 -0300

    add skeleton of bitvector class

---
+++ lib/BitVector.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <math.h>
+
+#include <utility>
+#include <vector>
+
+#include "glog/logging.h"
+#include "lib/Array.h"
+#include "lib/BitArray.h"
+
+namespace compact {
+namespace lib {
+
+class BitVector : public Array {
+ public:
+  BitVector() : bit_stream() {}
+
+  BitVector(uint n) { resize(n); }
+
+  BitVector(const std::initializer_list<uint>& values) {
+    reset(std::vector<uint>(values));
+  }
+
+  template <typename ArrayType>
+  BitVector(const ArrayType& values) {
+    reset(values);
+  }
+
+  void resize(uint n) { bit_stream.resize(n); }
+
+  template <typename ArrayType>
+  void reset(const ArrayType& values) {
+    bit_stream.reset(values);
+  }
+
+  void assign(uint n, uint val) { bit_stream.assign(n, val); }
+
+  uint size() const override { return bit_stream.size(); }
+
+  uint read(uint idx) const override { return bit_stream.read(idx); }
+
+  void write(uint idx, uint val) override { bit_stream.write(idx, val); }
+
+  // TODO
+  uint rank(uint pos) const { return 0; }
+
+  // TODO
+  uint select(uint idx) const { return 0; }
+
+  std::string to_string() const { return bit_stream.to_string(); }
+
+ private:
+  BitArray bit_stream;
+};
+
+}  // namespace lib
+}  // namespace compact
---
+++ lib/tests/BitVectorTest.cpp
@@ -0,0 +1,27 @@
+#include <exception>
+#include <vector>
+
+#include "glog/logging.h"
+#include "gtest/gtest.h"
+#include "lib/BitVector.h"
+
+namespace compact {
+namespace lib {
+namespace test {
+
+// test BitVector
+TEST(BitVectorTest, bitVectorTest) {
+  /*
+    init
+  */
+  BitVector bitv;
+  BitVector bitv2({1, 1, 0});
+
+  EXPECT_EQ(bitv.size(), 0);
+  EXPECT_EQ(bitv2[0], 1);
+  EXPECT_EQ(bitv2[1], 1);
+  EXPECT_EQ(bitv2[2], 0);
+}
+}  // namespace test
+}  // namespace lib
+}  // namespace compact
